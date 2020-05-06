ac6bd05684c1bf6b83de99c9488495f2811d6cd0
touste <touste@users.noreply.github.com>
2020-05-06 23:44:41 +0200

    Update interpolation.jl

--- src/interpolation.jl
+++ src/interpolation.jl
@@ -16,6 +16,8 @@ function evaluate(interp::AbstractInterpolation, boundary::HyperRectangle, point
     evaluate(interp, coords)
 end
 
+
+
 function fieldgradient(cell::Cell{D}, point::AbstractArray) where {D <: AbstractInterpolation}
     leaf = findleaf(cell, point)
     fieldgradient(leaf.data, leaf.boundary, point)
@@ -28,4 +30,19 @@ end
 
 function fieldgradient(itp::AbstractInterpolation, point::AbstractArray)
     gradient(itp, point...)
+end
+
+
+function fieldhessian(cell::Cell{D}, point::AbstractArray) where {D <: AbstractInterpolation}
+    leaf = findleaf(cell, point)
+    fieldhessian(leaf.data, leaf.boundary, point)
+end
+
+function fieldhessian(interp::AbstractInterpolation, boundary::HyperRectangle, point::AbstractArray)
+    coords = (point - boundary.origin) ./ boundary.widths .+ 1
+    fieldhessian(interp, coords) ./ boundary.widths
+end
+
+function fieldhessian(itp::AbstractInterpolation, point::AbstractArray)
+    hessian(itp, point...)
 end
